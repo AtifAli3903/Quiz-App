04921bcbdec5461d42c0112b197877874ec34a67
AtifAli3903 <95828829+AtifAli3903@users.noreply.github.com>
2022-02-07 19:34:39 +0500

    Add files via upload

---
+++ Quiz App java Script/public/questions.js
@@ -0,0 +1,34 @@
+var questions = [
+  ["1.Who was the first President of the Constituent Assembly of Pakistan?", "A. Liaquat Ali Khan", "B. Quaid-e-Azam", "C. Moulvi Tameez-ud-Din","D. Sardar Abdur Rab Nishtar", "B. Quaid-e-Azam"],
+	
+	
+  ["2.After how many years Pakistan got its first constitution?", "A. 5 years", "B. 7 years", " C. 9 years","D. 11 years"," C. 9 years"],
+	
+	
+  ["3.What document was firstly drafted to give pace to constitution making process?", "A. Representative Act", "B. Pakistan Act", "C. Independence Act", "D. Objective Resolution","D. Objective Resolution"],
+	
+	
+  ["4.When the Constituent Assembly passed the Objective Resolution?", " A. 14th February 1949", " B. 12th March 1949", "C. 9th June 1949", "D. 15th August 1949"," B. 12th March 1949"],
+	
+	
+  ["5.When Mohammad Ali Bogra presented Bogra Formula in the assembly?", " A. January 1953", "B. April 1953", "C. September 1953","D. October 1953","D. October 1953"],
+	
+	
+  ["6.Who was Mohammad Ali Bogra?", "A. Prime Minister", "B. Foreign Minister", "C. Law Minister", "D. Parliament Minister","A. Prime Minister"],
+	
+	
+  ["7.What is the other name of Mohammad Ali Bogra Formula?", "A. New Law of Pakistan", "B. Pakistan Report", "C. Third Report", "D. Constitutional Formula", "D. Constitutional Formula"],
+	
+	
+  ["8.When first constitution of Pakistan was enforced?", "A. 8th June 1956", "B. 23rd March 1956", "C. 14th August 1956", "D. 25th December 1956", "B. 23rd March 1956"],
+	
+	
+  ["9.Who was the Prime Minister of Pakistan during enforcement of first constitution?", "A. Mohammad Ali Bogra", "B. Khwaja Nazim Uddin", "C. Choudhry Mohammad Ali", "D. Ibrahim Ismail Chundrigar","C. Choudhry Mohammad Ali"],
+	
+	
+  ["10.What official name was given to Pakistan in 1956 constitution?", "A. United States of Pakistan", "B. Republic of Pakistan", "C. Islamic Pakistan", "D. Islamic Republic of Pakistan","C. Islamic Pakistan"],
+	
+	
+  ["11.Kesa laga mera Mcqs Ka Paper ?? By.Atif Ali", "A.Acha", "B.Bura", "C.Bhot acha", "D.Maza nhi aya","C.Bhot acha"],
+
+  ];
